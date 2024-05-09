39e10c253a678dc43c80413c9217f71bec89260a
Kevin Alejandro <lealtorreskevin@gmail.com>
2024-05-08 23:48:49 -0600

    arreglos en el .gitignore

--- agendar-cita/src/main/java/com/doctor/Main.java
+++ agendar-cita/src/main/java/com/doctor/Main.java
@@ -9,7 +9,7 @@ public class Main {
         Doctor doctor = new Doctor("1", "Dr. Smith", Especialidades.GENERAL);
         Paciente paciente = new Paciente("1", "John Doe");
 
-        Cita cita = new Cita("1", "2024-05-09T10:00", "Consulta general", doctor, paciente);
+        Cita cita = new Cita("1", "2022-06-09T10:00", "Consulta generalisima", doctor, paciente);
         calendario.agregarCita(cita);
 
         calendario.exportarAJSON(ARCHIVO_JSON);
