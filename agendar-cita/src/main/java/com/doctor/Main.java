b812fbb3142815b072109ee49d9e1e1e8e0e5b68
Kevin Alejandro <lealtorreskevin@gmail.com>
2024-05-13 15:55:12 -0600

    Ultimos retoques

--- agendar-cita/src/main/java/com/doctor/Main.java
+++ agendar-cita/src/main/java/com/doctor/Main.java
@@ -7,9 +7,9 @@ public class Main {
         Calendario calendario = Calendario.cargarDesdeJSON(ARCHIVO_JSON);
 
         Doctor doctor = new Doctor("1", "Dr. Smith", Especialidades.GENERAL);
-        Paciente paciente = new Paciente("1", "John Doe");
+        Paciente paciente = new Paciente("2", "kevin");
 
-        Cita cita = new Cita("1", "2022-06-09T10:00", "Consulta generalisima", doctor, paciente);
+        Cita cita = new Cita("3", "2023-06-09T10:00", "Consulta de rompedur de algo", doctor, paciente);
         calendario.agregarCita(cita);
         calendario.exportarAJSON(ARCHIVO_JSON);
     }
